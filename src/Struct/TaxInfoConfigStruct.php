8add6cecd880362b721b769f0b7b4bf097b32533
M4MEET <imeetjoshi@gmail.com>
2024-06-24 17:24:28 +0200

    Add new struct for tax info configuration

--- src/Struct/TaxInfoConfigStruct.php
+++ src/Struct/TaxInfoConfigStruct.php
@@ -4,46 +4,34 @@ namespace ThemeBattronGmbh\Struct;
 
 use Shopware\Core\Framework\Struct\Struct;
 
-/**
- * Class TaxInfoConfigStruct
- *
- * This class extends the Shopware's Struct class and is used to hold the configuration for tax information.
- * It has a single property, $taxEntity, which represents the tax entity ID selected in the plugin configuration.
- */
 class TaxInfoConfigStruct extends Struct
 {
-    /**
-     * @var ?string The tax entity ID selected in the plugin configuration.
-     */
     protected ?string $taxEntity;
+    protected ?string $taxInfoText;
 
-    /**
-     * TaxInfoConfigStruct constructor.
-     *
-     * @param ?string $taxEntity The tax entity ID selected in the plugin configuration.
-     */
-    public function __construct(?string $taxEntity)
+    public function __construct(?string $taxEntity, ?string $taxInfoText)
     {
         $this->taxEntity = $taxEntity;
+        $this->taxInfoText = $taxInfoText;
     }
 
-    /**
-     * Gets the tax entity ID.
-     *
-     * @return ?string The tax entity ID.
-     */
     public function getTaxEntity(): ?string
     {
         return $this->taxEntity;
     }
 
-    /**
-     * Sets the tax entity ID.
-     *
-     * @param ?string $taxEntity The tax entity ID.
-     */
+    public function getTaxInfoText(): ?string
+    {
+        return $this->taxInfoText;
+    }
+
     public function setTaxEntity(?string $taxEntity): void
     {
         $this->taxEntity = $taxEntity;
+    }
+
+    public function setTaxInfoText(?string $taxInfoText): void
+    {
+        $this->taxInfoText = $taxInfoText;
     }
 }
